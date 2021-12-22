f64c7465f2ec5c5fc47fbfb3ede107bd79822cce
JJiWoNing <jjiwoning12@gmail.com>
2021-12-22 17:44:40 +0900

    VariableType

--- src/VariableType.java
+++ src/VariableType.java
@@ -6,11 +6,12 @@ public class VariableType {
         double b = 5.5;
         char c = 'a';
         boolean d = true;
+        String e = "Hello Java";
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
         System.out.println("c = " + c);
         System.out.println("d = " + d);
-        
+        System.out.println("e = " + e);
     }
 }
