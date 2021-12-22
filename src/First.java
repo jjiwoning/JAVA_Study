f4e3197882bb9d48f44efc02732e9c0779034b17
JJiWoNing <jjiwoning12@gmail.com>
2021-12-22 16:47:08 +0900

    Initial commit

---
+++ src/First.java
@@ -0,0 +1,9 @@
+public class First {
+    public static void main(String[] args){
+        System.out.println("Hello, JAVA World!");
+        int i = 10;
+        System.out.println("i =" + i);
+        i = 100;
+        System.out.println("i =" + i);
+    }
+}
