99627e36be703625c9b19b6d5032d77b50043a63
JJiWoNing <jjiwoning12@gmail.com>
2021-12-22 17:39:09 +0900

    VariableType

--- src/VariableType.java
+++ src/VariableType.java
@@ -1,7 +1,16 @@
+import javax.sound.midi.Soundbank;
+
 public class VariableType {
     public static void main(String[] args) {
         int a = 5;
-        int b = 10;
-        System.out.println(a+b);
+        double b = 5.5;
+        char c = 'a';
+        boolean d = true;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        
     }
 }
