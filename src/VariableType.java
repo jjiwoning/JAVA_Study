dc3b51850d3d5733b661cc530d215918af527147
JJiWoNing <jjiwoning12@gmail.com>
2021-12-22 17:54:12 +0900

    VariableType

--- src/VariableType.java
+++ src/VariableType.java
@@ -13,5 +13,15 @@ public class VariableType {
         System.out.println("c = " + c);
         System.out.println("d = " + d);
         System.out.println("e = " + e);
+        
+        // 형 변환 예시
+        // 자동 형 변환 -> 작은 공간에서 큰 공간으로
+        byte by = 10;
+        int in = by;
+        System.out.println("in = " + in);
+        // 명시적 형 변환 -> 큰 공간에서 작은 공간으로 -> 데이터 누실 될 수 있다.
+        int iVar = 123456;
+        byte bVar = (byte)iVar;
+        System.out.println("bVar = " + bVar);
     }
 }
