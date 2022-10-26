f568011730c0744e70454c4ed53c87176866208f
JJiWoNing <jjiwoning12@gmail.com>
2022-10-26 21:26:31 +0900

    10/26 study

--- test/WrapperClass/WrapperClassTest.java
+++ test/WrapperClass/WrapperClassTest.java
@@ -13,7 +13,16 @@ public class WrapperClassTest {
         Integer c = new Integer(7);
         Integer d = new Integer(7);
         int e = 7;
+
         System.out.println(c == d);
         System.out.println(c == e);
+    }
+
+    @Test
+    void LongTest() {
+        Long a = 1L;
+        Long b = 1L;
+
+        System.out.println("(a == b) = " + (a == b));
     }
 }
